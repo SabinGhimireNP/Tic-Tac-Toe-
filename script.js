7d8e1fed5abe6a0ec953c62b460b838ea7e9a76f
Sabin Ghimire <sabinghimire223@gmail.com>
2025-07-29 08:36:37 +0545

    data saved

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const tiles = document.querySelectorAll(".tile");
+const board = ["", "", "", "", "", "", "", "", ""];
 let player1 = Math.round(Math.random());
 function contentChange(e, tile) {
   e.target.classList.add("active");
+  let index = e.target.dataset.set - 1;
   setTimeout(() => {
     if (!tile.innerHTML) {
       tile.innerHTML = player1
         ? `<i class="fa-solid fa-xmark" style="color: #ea5454;"></i>`
         : `<i class="fa-regular fa-circle" style="color: #238090;"></i>`;
+      let currentPlayer = player1 ? "X" : "O";
+      board[index] = currentPlayer;
       player1 = !player1;
     }
   }, 300);
